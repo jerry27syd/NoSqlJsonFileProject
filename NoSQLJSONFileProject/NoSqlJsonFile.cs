1672360345e15598435e6f69998aed609674dc99
Jerry <jerry27syd@gmail.com>
2013-03-13 16:46:42 +1100

    rf

--- NoSQLJSONFileProject/NoSqlJsonFile.cs
+++ NoSQLJSONFileProject/NoSqlJsonFile.cs
@@ -5,8 +5,9 @@
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   any later version.                                                    *
  ***************************************************************************/
+//version : 1.0.0313
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -31,7 +32,7 @@ namespace NoSqlJsonFileProject
         ///  <appSettings>
         ///    <add key="FilePath" value="C:\temp"/>
         ///  </appSettings>
-        /// </summary>
+        /// </summary>who 
         public static string FILE_PATH = ConfigurationManager.AppSettings["FilePath"] ?? @"NoSqlJsonFiles";
         
         /// <summary>
