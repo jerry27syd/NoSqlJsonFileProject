ee238fd7bfe105f1d1608d224497366d985bd91b
Jerry <jerry27syd@gmail.com>
2013-03-13 11:45:23 +1100

    Test checkin

--- NoSQLJSONFileProject/NoSqlJsonFile.cs
+++ NoSQLJSONFileProject/NoSqlJsonFile.cs
@@ -7,7 +7,6 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
